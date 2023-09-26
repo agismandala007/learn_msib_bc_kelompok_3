1456b3fc2313f6a747706a0a4fba087e423a9162
agismandala007 <agixsatria@gmail.com>
2023-09-26 22:23:27 +0700

    test

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -2,6 +2,23 @@ pragma solidity >=0.4.0 <0.9.0;
 
 contract Identity {
     uint age = 22;
-    string name = "Agis";
-    string email = "agis@gmail.com";
+    string username = "agismandala";
+    string private _email = "agisdrive13@gmail.com";
+
+    struct Blockchain {
+        uint id;
+        string name;
+    }
+
+    Blockchain[] public blockchains;
+
+    function getEmail() public view returns (string memory) {
+        return _email;
+    }
+
+    function _createBlockchain(uint _id, string memory _name) private {
+        blockchains.push(Blockchain(_id, _name));
+    }
 }
+
+
